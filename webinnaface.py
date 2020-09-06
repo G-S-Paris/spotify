c811e4eaaf9b823626de368cd93f41448b21e0e3
Greyson Paris <greyson.paris@protonmail.com>
2020-09-06 16:14:26 -0400

    adding basic tests to prove vscode is setup; using environment variables to track auth details

--- webinnaface.py
+++ webinnaface.py
@@ -1,15 +1,16 @@
-# import requests
-
-# r = requests.get()
+import os
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        client_id="id",
-        client_secret="secret",
+        client_id=os.getenv("TestifyClientID"),
+        client_secret=os.getenv("TestifyClientSecret"),
         redirect_uri="http://127.0.0.1:9090",
         scope="user-library-read",
     )
 )
+results = sp.search(q="weezer", limit=20)
+for idx, track in enumerate(results["tracks"]["items"]):
+    print(idx, track["name"])
